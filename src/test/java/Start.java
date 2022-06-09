a0d5c575b524a3dc178863f4caeaeb5feee0f10b
RahulJadhwani1903 <92024230+RahulJadhwani1903@users.noreply.github.com>
2022-06-09 16:51:03 +0200

    second

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -3,6 +3,8 @@ import org.testng.annotations.Test;
 public class Start {
     @Test
     public static void startProject() {
+
         System.out.println(">>>>>>>>>>>>>  Started  <<<<<<<<<<<<<<<");
+        System.out.println(">>>>>>>>>>>>>  Ended  <<<<<<<<<<<<<<<");
     }
 }
